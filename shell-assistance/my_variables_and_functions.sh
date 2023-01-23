991ec5fbec54f28c187132aadc0cf4636970944e
Sergey Serov <me@sergey-serov.ru>
2023-01-23 20:39:54 +0300

    Dinamic CVS info is ready.

--- shell-assistance/my_variables_and_functions.sh
+++ shell-assistance/my_variables_and_functions.sh
@@ -31,7 +31,7 @@ TURQUOISE='\e[0;36m'
 WHITE='\e[0;37m'
 LIGHT_GRAY='\e[37m'
 DARK_GRAY='\e[90m'
-###echo -e "\e[38;5;82mHello \e[38;5;198mWorld"
+### echo -e "\e[38;5;82mHello \e[38;5;198mWorld"
 
 # Text format
 BOLD='\e[1m'
@@ -44,32 +44,35 @@ PURPLE_BG='\e[45m'
 
 
 ##
-# Horizont line for PS0 and PS1
+# Horizont line for PS1
 #
 
-horisont_1 () {
+horisont_1 () {  
+
     HORIZONT_1=
     local CONSOLE_WIDTH=$(($COLUMNS)) # place for time
+
     # create line
     while [ $CONSOLE_WIDTH -gt 0 ]
     do
         HORIZONT_1="${HORIZONT_1}―" # Unicode U+2015 "Horisontal bar"
         CONSOLE_WIDTH=$(($CONSOLE_WIDTH-1))
     done
+
     # add time at the end
     HORIZONT_1="${HORIZONT_1}" #  [\t]
 }
 
+##
+# Horizont line for PS0
+#
 horisont_2 () {
 
     HORIZONT_2=
 
     local DATE_PATTERN="%A | %d %B | %Y | %T | week %U"
-    # echo 'debug 1: ' $DATE_PATTERN
     local DATE_SYMBOLS=$(printf "%(${DATE_PATTERN})T" -1 | wc -m)
-    # echo 'debug 2: ' $DATE_SYMBOLS
     local CONSOLE_WIDTH=$(($COLUMNS-$DATE_SYMBOLS-15)) # place for time, date and command number
-    # echo 'debug 3: ' $CONSOLE_WIDTH
 
     # create line
     while [ $CONSOLE_WIDTH -gt 0 ]
@@ -77,6 +80,7 @@ horisont_2 () {
         HORIZONT_2="${HORIZONT_2}-" # Unicode U+2015 "Horisontal bar"
         CONSOLE_WIDTH=$(($CONSOLE_WIDTH-1))
     done
+
     # add time at the end
     HORIZONT_2="${HORIZONT_2} \D{"${DATE_PATTERN}"} | command \#"
 }
@@ -86,15 +90,35 @@ horisont_2 () {
 #
 
 repo_info () {
+
     IS_REPO=
     REPO_INFO=
+    REPO_DESCRIPTION=
 
+    # branch
     IS_REPO=$(__git_ps1)
     if [ -n "$IS_REPO" ]
-        then REPO_INFO=$BOLD$IS_REPO$COLOR_END
+    then 
+        REPO_INFO=$BLUE$IS_REPO$COLOR_END
     fi
-    #  add green or red color TODO
 
+    # repo description
+    if [[ -d .git && -f .git/description ]]
+    then
+        local REPO_DESCRIPTION=$(cat .git/description)
+    fi
+  
+    if echo "$REPO_DESCRIPTION" | grep "Unnamed repository" > /dev/null
+    then
+        REPO_DESCRIPTION="${YELLOW} no description... ${COLOR_END}"
+    else
+        REPO_DESCRIPTION="${TURQUOISE} ${REPO_DESCRIPTION} ${COLOR_END}"
+    fi
+
+    # final
+    REPO_INFO="${REPO_INFO} ${REPO_DESCRIPTION}"
+
+    echo -en $REPO_INFO
 }
 
 
@@ -102,17 +126,21 @@ repo_info () {
 # Init my variables for PS*
 #
 
-init_my_variables () {
+init_prompt_variables () {
+
+    # set +x
+
     horisont_1
     horisont_2
-    repo_info
+
+    # set -x
 }
 
 
 ##
-# PS1
+# Is success or fail in previous command ?
 #
-get_previos_command_result () {
+get_previos_command_result () { # todo not works! always 0
     if [ $? -eq 0 ]
     then
         PREVIOUS_COMMAND_RESULT="completed"
@@ -123,21 +151,29 @@ get_previos_command_result () {
     echo $PREVIOUS_COMMAND_RESULT
 }
 
-init_my_variables
-PROMPT_COMMAND=init_my_variables
+##
+# Run before every prompt
+#
+
+init_prompt_variables
+PROMPT_COMMAND=init_prompt_variables
+
+##
+# PS1
+#
 
 USER_AND_HOST="[${YELLOW}\u${COLOR_END}${BLUE}@${COLOR_END}${TURQUOISE}\h${COLOR_END}]"
 CURRENT_DIR="${DARK_GRAY}\$(pwd)${COLOR_END}"
 
 
-PS1="${PURPLE}--- $(get_previos_command_result). ${COLOR_END}\n${GREEN}${HORIZONT_1}${COLOR_END}\n${USER_AND_HOST} ${CURRENT_DIR} ${REPO_INFO}\n🌌 "
+PS1="${PURPLE}--- $(get_previos_command_result). ${COLOR_END}\n${GREEN}${HORIZONT_1}${COLOR_END}\n${USER_AND_HOST} ${CURRENT_DIR} \$(repo_info)\n🌌 "
 
 
 ##
 # PS0
 #
 
-PS0="${PURPLE}${HORIZONT_2}${COLOR_END}\n"
+PS0="${PURPLE}${HORIZONT_2}${COLOR_END}\n\n"
 
 
 # PS1="${GREEN}[\u@\h] \$(pwd) ============================================ [\t]${COLOR_END}\n🌌 "
@@ -170,5 +206,5 @@ PS0="${PURPLE}${HORIZONT_2}${COLOR_END}\n"
 # 💐🌳
 
 
-
+    #  add green or red color TODO in cvs status
 
