286be51cf9665405bfaa1a19c1cac7e1acde8faa
Sergey Serov <me@sergey-serov.ru>
2023-01-30 19:03:13 +0300

    Try script which will runs from ~/bin like program by symbolic link.

---
+++ bin/p.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# author: Sergey Serov
+
+# DESCRIPTION
+#############
+
+# HELP
+######
+
+# CONFIG
+########
+
+CURRENT_COMMAND=$1
+
+# PROGRAM
+#########
+
+# TODO
+######
+
+# WAREHOUSE
+###########
+
+# todo!!
+echo '-----------'
+echo "[`date +"%T"`]: $CURRENT_COMMAND"
+echo '-----------'
+$CURRENT_COMMAND
+
+
+
+
