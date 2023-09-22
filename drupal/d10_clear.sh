2c70bc64edfa35a773c7dc6d71a811e49c768409
Sergey Serov <me@sergey-serov.ru>
2023-09-22 17:28:23 +0300

    Refactoring script for Drupal 10 clear cache during development. Not tested!

--- drupal/d10_clear.sh
+++ drupal/d10_clear.sh
@@ -1,23 +1,63 @@
 #!/bin/bash
-set -x
-rm -fr /var/www/experiment1/www/sites/default/files/php/twig/*
 
-mysql -e "
-use experiment1;
-truncate table cache_bootstrap;
-truncate table cache_config;
-truncate table cache_container;
-truncate table cache_data;
-truncate table cache_default;
-truncate table cache_discovery;
-truncate table cache_discovery_migration;
-truncate table cache_entity;
-truncate table cache_jsonapi_normalizations;
-truncate table cache_menu;
-truncate table cache_migrate;
-truncate table cache_render;
-truncate table cache_rest;
-truncate table cache_toolbar;
-"
+# author: Sergey Serov
 
-set +x
+# DESCRIPTION
+#############
+# Clear Drupal 10 all caches.
+# This script maps to hot keys and runs in background.
+# Result outputs in log.
+
+# HELP
+######
+
+# CONFIG
+########
+LOG='/tmp/drupal10_clear.log'
+
+# PROGRAM
+#########
+
+curl -I "http://$CURRENTLY_SITE/core/rebuild.php" >> $LOG
+
+curl -s "http://$CURRENTLY_SITE/core/rebuild.php"
+
+
+# TODO
+######
+# NOT TESTED!
+# add lock switcher
+# add key!
+
+# WAREHOUSE
+###########
+
+# if [[  ]]
+# then
+#     echo 'Cache was cleared.' >> /tmp/drupal10_clear.log
+# fi
+
+# curl -I "http://$CURRENTLY_SITE/core/rebuild.php" >> /tmp/drupal10_clear.log
+
+# set -x
+# rm -fr /var/www/$CURRENTLY_SITE/www/sites/default/files/php/twig/*
+
+# mysql -e "
+# use $CURRENTLY_DATABASE;
+# truncate table cache_bootstrap;
+# truncate table cache_config;
+# truncate table cache_container;
+# truncate table cache_data;
+# truncate table cache_default;
+# truncate table cache_discovery;
+# truncate table cache_discovery_migration;
+# truncate table cache_entity;
+# truncate table cache_jsonapi_normalizations;
+# truncate table cache_menu;
+# truncate table cache_migrate;
+# truncate table cache_render;
+# truncate table cache_rest;
+# truncate table cache_toolbar;
+# "
+
+# set +x
