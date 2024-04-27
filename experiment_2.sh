9a35654133b7c4c86b5ad9b3e6827dffb47cc4c5
Sergey Serov <me@sergey-serov.ru>
2024-04-27 15:17:23 +0300

    Added helper to clean Rust projects.

--- experiment_2.sh
+++ experiment_2.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+
+# https://unix.stackexchange.com/questions/295965/how-to-use-find-exec-to-execute-command-in-directory-of-found-file-not-curre
+# works not correct, but result is ok.
+# todo finished!
+find /home/sergey/Forge/rust/* -maxdepth 0 -name '.git' -type d -prune -o -type d -exec sh - 'pwd' {} \;
